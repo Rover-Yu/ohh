a0addc5889fb596198c82d2aef77287d0dbd0db1
Li Yu <raise.sail@gmail.com>
2011-05-29 10:48:43 +0800

    rebuilddb.qemu-kvm.sh: run unifdef OBSOLETE_KVM_IMPL

--- rebuilddb.qemu-kvm.sh
+++ rebuilddb.qemu-kvm.sh
@@ -2,6 +2,11 @@
 
 find -name "*.[chsS]" | grep -v -E  "(sparc)|(arm)|(microblaze)|(powerpc)|(ppc)|(s390)|(mips)|(cris)|(m68k)|(alpha)|(bsd-user)|(darwin-user)|(linux-user)|(ppc)|(sh4)|(ia64)|(tests)|(target-i386/kvm.c)|(cpus.c)|(kvm-all.c)|(kvm.h)|(kvm/)|(kvm-stub.c)|(win32)" > cscope.files
 
+unifdef -U OBSOLETE_KVM_IMPL target-i386/kvm.c > target-i386/kvm.c.newkvm.c
+unifdef -U OBSOLETE_KVM_IMPL cpus.c > cpus.c.newkvm.c
+unifdef -U OBSOLETE_KVM_IMPL kvm-all.c > kvm-all.c.newkvm.c
+unifdef -U OBSOLETE_KVM_IMPL kvm.h  > kvm.h.newkvm.h
+
 echo "./target-i386/kvm.c.newkvm.c" >> cscope.files
 echo "./cpus.c.newkvm.c" >> cscope.files
 echo "./kvm-all.c.newkvm.c" >> cscope.files
