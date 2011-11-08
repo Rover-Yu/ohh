ce5464cf4675d7744397b9618dcf2072bb04dc94
Li Yu <raise.sail@gmail.com>
2011-11-08 14:03:23 +0800

    add *.[chS] glob to filter source code, elsewise cscope will crash

--- rebuilddb.kernel.sh
+++ rebuilddb.kernel.sh
@@ -5,18 +5,15 @@ set -x
 rm -f *cscope*
 rm -f *tags*
 
-find arch/x86/ block/ crypto/ init/ ipc/ kernel/ lib/ mm virt/ >> cscope.files
-find drivers/ | grep -E -e '(/acpi/)|(/base/)|(/block/)|(ers/char/)|(clocksource/)|(dma/)|(/net/igb/)|(/pci/)|(net/loopback.c)' >> cscope.files
-find fs/ | grep -E -e "(btrfs)|(fs/configfs/)|(fs/debugfs/)|(/ext[234]/)|(hugetlbfs)|(notify)|(fs/proc)|(/sysfs)|(cachefs)" >> cscope.files 
-find include/ >> cscope.files
-find net/ |grep -E -e "(^net/core)|(^net/sched)|(^net/ethernet)|(^net/ipv4)|(^net/unix)|(^net/netfilter)|(^net/netlink)" >> cscope.files 
+find arch/x86/ block/ crypto/ init/ ipc/ kernel/ lib/ mm virt/ -name "*.[chS]">> cscope.files
+find drivers/ -name "*.[chS]" | grep -E -e '(/acpi/)|(/base/)|(/block/)|(ers/char/)|(clocksource/)|(dma/)|(/net/igb/)|(/pci/)|(net/loopback.c)' >> cscope.files
+find fs/ -name "*.[chS]" | grep -E -e "(btrfs)|(fs/configfs/)|(fs/debugfs/)|(/ext[234]/)|(hugetlbfs)|(notify)|(fs/proc)|(/sysfs)|(cachefs)" >> cscope.files 
+find include/ -name "*.[chS]" >> cscope.files
+find net/ -name "*.[chS]" | grep -E -e "(^net/core)|(^net/sched)|(^net/ethernet)|(^net/ipv4)|(^net/unix)|(^net/netfilter)|(^net/netlink)" >> cscope.files 
 ls net/*.c >> cscope.files 
 ls net/*.h >> cscope.files
 ls fs/*.c >> cscope.files 
 ls fs/*.h >> cscope.files
-
-grep -E "/.+\." cscope.files > cscope.files.no-folders
-mv cscope.files.no-folders cscope.files
 
 ctags --excmd=number --fields=afiKlmnSzt -L cscope.files
 
