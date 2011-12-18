63c39e817c813afc7b5276609ed47210287aa43a
Li Yu <raise.sail@gmail.com>
2011-12-18 09:47:40 +0800

    rebuilddb.kernel.sh: tune for drivers/net/* of upstream kernel changes, and add net/bridge/

--- rebuilddb.kernel.sh
+++ rebuilddb.kernel.sh
@@ -6,10 +6,10 @@ rm -f *cscope*
 rm -f *tags*
 
 find arch/x86/ block/ crypto/ init/ ipc/ kernel/ lib/ mm virt/ -name "*.[chS]">> cscope.files
-find drivers/ -name "*.[chS]" | grep -E -e '(/acpi/)|(/base/)|(/block/)|(ers/char/)|(clocksource/)|(dma/)|(/net/igb/)|(/pci/)|(net/loopback.c)' >> cscope.files
+find drivers/ -name "*.[chS]" | grep -E -e '(/acpi/)|(/base/)|(/block/)|(ers/char/)|(clocksource/)|(dma/)|(/net/.*/igb/)|(/pci/)|(net/[^/]+.c)' >> cscope.files
 find fs/ -name "*.[chS]" | grep -E -e "(btrfs)|(fs/configfs/)|(fs/debugfs/)|(/ext[234]/)|(hugetlbfs)|(notify)|(fs/proc)|(/sysfs)|(cachefs)" >> cscope.files 
 find include/ -name "*.[chS]" >> cscope.files
-find net/ -name "*.[chS]" | grep -E -e "(^net/core)|(^net/sched)|(^net/ethernet)|(^net/ipv4)|(^net/unix)|(^net/netfilter)|(^net/netlink)" >> cscope.files 
+find net/ -name "*.[chS]" | grep -E -e "(^net/core)|(^net/sched)|(^net/ethernet)|(^net/ipv4)|(^net/unix)|(^net/netfilter)|(^net/netlink)|(^net/bridge)" >> cscope.files 
 ls net/*.c >> cscope.files 
 ls net/*.h >> cscope.files
 ls fs/*.c >> cscope.files 
